f46639f6749fcbc04c04a5f4b1b5df1c880d350b
Alexander Kurbatov <sir.alkurbatov@yandex.ru>
2022-11-20 00:35:42 +0300

    feat: Implement metrics Agent
    Gather required metrics and send them to the server.
    
    Signed-off-by: Alexander Kurbatov <sir.alkurbatov@yandex.ru>

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"context"
+
+	"github.com/alkurbatov/metrics-collector/internal/app"
+	"github.com/alkurbatov/metrics-collector/internal/logging"
+	"github.com/alkurbatov/metrics-collector/internal/metrics"
+)
+
+func main() {
+	app := app.NewAgent()
+	ctx := context.Background()
+
+	stats := metrics.NewMetrics()
+	go app.Poll(ctx, stats)
+	go app.Report(ctx, stats)
+
+	logging.Log.Info("Agent has started")
+	select {}
+}
